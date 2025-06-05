ec40085c31131ac94a8c48c31f3fa1f43046735d
Vladislav Bogdanov <vla-bogdano@ya.ru>
2025-06-05 17:12:24 +0300

    Добавлен свой базовый проект в папку примеров на основе hello_world

---
+++ samples/my_project/src/main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+	printf("my_project! %s\n", CONFIG_BOARD_TARGET);
+	while ( 1 )
+	{
+		k_msleep(1000);
+		printf("my_project! %s\n", CONFIG_BOARD_TARGET);
+	}
+	
+	return 0;
+}
